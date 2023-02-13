d640da979b2c28738b39e1f1f6d78f68138bf90a
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-02-12 21:52:21 -0300

    request working :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NOTES } from 'src/data';
-import {Note} from "../model/note"
+import { Note } from '../model/note';
 import { NoteCardComponent } from './note-card/note-card.component';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'noteThonker_front';
+  notes: Note[];
 
-  notes = NOTES
+  getNotes(){
+    this.http.get("http://localhost:8080/notes").subscribe(
+      data => {
+        this.notes = <Note[]>data
+      })
+  }
+  
+  ngOnInit() {
+    this.getNotes()
+  }
+  constructor(private http: HttpClient) {}
 
+  onGetNotes() { 
+    this.getNotes()
+  }
 }
