96fd4833d4f70d76b295eb02a7543ad561ccebc3
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-02-25 22:18:19 -0300

    Add search bar functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
-import {Observable, concat, flatMap, of} from 'rxjs';
+import { Component } from '@angular/core';
+import {Observable,concat, of, tap, debounceTime, switchMap, BehaviorSubject, distinctUntilChanged} from 'rxjs';
 import { Note } from '../model/note';
 import {NoteService} from './note-service.service';
 
@@ -9,29 +9,35 @@ import {NoteService} from './note-service.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	title = 'noteThonker_front';
-  notes:Note[]
-  notes$: Observable<Note[]>
+    title = 'noteThonker_front';
+    notes:Note[] = []
+    notes$: Observable<Note[]> = of(this.notes)
+    getNotesObservable: Observable<Note[]>
+    searchText$ = new BehaviorSubject("");
+    searchSubscription = this.searchText$.pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        switchMap(text => this.noteService.searchNotes(text)),
+        tap(data=> {
+            this.notes = data
+            console.log(data)
+        })
+    ).subscribe()
 
-	executeRequest(incRequest$:Observable<unknown | Note>){
-	concat(incRequest$, this.noteService.getNotes()).subscribe(
-					data => {
-									this.notes = (data as Note[])
-					}
-	)	
-
-	}
-	trackItem(index:Number, note:Note) {
-			return note.id
-	}
-	constructor(
-			private noteService:NoteService,
-			) {
-				noteService.getNotes().subscribe(
-								data => this.notes = data
-				)
-				this.notes$ = of(this.notes)
-			}
-
-
+    executeRequest(incRequest$:Observable<unknown | Note>){
+        concat(incRequest$, this.getNotesObservable).subscribe()
+    }
+    searchNotes(e:any){
+        this.searchText$.next(e.target.value)
+    }
+    constructor(private noteService:NoteService,) {
+        this.getNotesObservable = noteService.getNotes().pipe(
+            tap(data => this.notes = data)
+        )
+        this.getNotesObservable.subscribe()
+    }
+    trackItem(index:Number, note:Note) {
+        return note.id
+    }
+ 
 }
--- src/app/note-service.service.ts
+++ src/app/note-service.service.ts
@@ -10,10 +10,13 @@ export class NoteService {
     private reqUrl:string = "http://localhost:8080/notes"
 
     public getNotes(): Observable<Note[]>{
-		return this.http.get<Note[]>(`${this.reqUrl}`)
-	}
-	public deleteNote(id:Number){
-		return this.http.delete(`${this.reqUrl}/${id}`)
-	}
+        return this.http.get<Note[]>(`${this.reqUrl}`)
+    }
+    public deleteNote(id:Number){
+	return this.http.delete(`${this.reqUrl}/${id}`)
+    }
+    public searchNotes(searchString:string) { 
+        return this.http.get<Note[]>(`${this.reqUrl}/search?searchString=${searchString}`)
+    }
     constructor(private http:HttpClient) { }
 }
